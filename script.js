1ebb2047fd37eb2aad75244d8a9df738e0419b61
喜树 <xishu@xishudeMacBook-Air.local>
2026-07-09 20:22:11 +0800

    更新网站

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// Reserved for future interactions.
+// 判断是不是第一次访问
+const firstVisit = localStorage.getItem("firstVisit");
+
+if (!firstVisit) {
+    localStorage.setItem("firstVisit", "true");
+} else {
+    document.body.classList.add("canSkip");
+}
+
+// 第二次进入可以点击跳过动画
+document.addEventListener("click", () => {
+    if (document.body.classList.contains("canSkip")) {
+        document.body.classList.add("skip");
+    }
+});
+
+// 页面切换翻页效果
+document.querySelectorAll(".page-link").forEach(link=>{
+
+    link.addEventListener("click",function(e){
+
+        e.preventDefault();
+
+        document.body.classList.add("page-flip");
+
+        setTimeout(()=>{
+
+            location.href=this.href;
+
+        },850);
+
+    });
+
+});
